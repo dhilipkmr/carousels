5518434de2ab2713c77f67e80af408dc0d3e2a22
dhilipkmr <dhilip1211@gmail.com>
2019-03-02 19:36:41 +0530

    comments

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,7 +9,7 @@ const VISIBLEIMAGES = 3;
 const DURATION = 750;
 
 const Carousel = (props) => {
-  const {imgList = [], img_width = IMG_WIDTH, img_height = IMG_HEIGHT, visibleImages = VISIBLEIMAGES, duration = DURATION} = props;
+  const {imgList = [], img_width = IMG_WIDTH, img_height = IMG_HEIGHT, visibleImages = VISIBLEIMAGES, duration = DURATION, autoNext = false, timeForNext = 3000} = props;
   /* Hooks Declarations Start*/
   const [currFirstImg, setCurrFirstImg] = useState(0);  // The Current Middle Element/Primary element of our carousel
   const [actualFirst, setActualFirst] = useState('');   // The Clicked Image when it is not the Immediate Next or Immediate Previous Image
@@ -25,34 +25,36 @@ const Carousel = (props) => {
   const elementsInRight = visibleImages - elementsInLeft;
 
   const constructVisibleItemsProps = () => {
-    const visibleItemsProps = {};
+    const visibleItemsProps = {}; // To store config for items that are visibile in the carousel
     visibleItemsProps.order = [];
-    let curr_center = currFirstImg;
-    let timesToIterate = 0;
-    let zIndex = - elementsInRight;
-    let xTranslate = img_width;
-    let zTranslate = 0;
+    let curr_center = currFirstImg; // Storing the Current Middle element in focus
+    let timesToIterate = 0; // To iterate through all visible number of images.
+    let zIndex = - elementsInRight; // We start from left to right and Zindex has to keep on increasing till middle then has to reduce.
+    let xTranslate = img_width; // To move the element with respect to x axis
+    let zTranslate = 0; // To reduce image size for images apart from center
     let opacity = 1;
-    const division = (img_width * (1.66 / elementsInLeft));
-    let opacityDivider = (0.7 / elementsInRight);
+    const division = (img_width * (1.66 / elementsInLeft)); // Specifies the length that next image has to move away from with resoect to current image (1.6 times the current image)
+    let opacityDivider = (0.7 / elementsInRight); // minimum opacity should be 0.3 (1-0.7)
     let rightEltCount = elementsInRight;
-    let leftEltCount = elementsInLeft;
+    let leftEltCount = elementsInLeft; // including middle element
     let curr_center_copy = curr_center;
+  
     while(timesToIterate < visibleImages ) {
       const styles = {};
       let currImgIndex;
-      let currImgIndexOnRight = true;
+      let currImgIndexOnRight = true; // Tells if in this iteration the currently iterated image lies left to the middle image or not
+      // To set properties for elements in right side
       if (timesToIterate < elementsInRight) {
         const nextIndex = curr_center - (rightEltCount);
-        currImgIndex = nextIndex > -1 ? nextIndex : imgList.length - Math.abs(nextIndex);
-        opacity = 1 - (opacityDivider * rightEltCount);
-        zTranslate =  -division * rightEltCount;
-        xTranslate = img_width - (division * rightEltCount);
+        currImgIndex = nextIndex > -1 ? nextIndex : imgList.length - Math.abs(nextIndex); // Gives the rightmost elemnt in first iteration and then the subsequent elements down the iteration
+        opacity = 1 - (opacityDivider * rightEltCount); // To assign lowest opacity to last element and increaing it till we come to middle
+        zTranslate =  -division * rightEltCount;  // To increase the size of the images subsequently from last to middle
+        xTranslate = img_width - (division * rightEltCount);  // X coordinate position
         rightEltCount--;
-      } else {
+      } else {  // To set properties for elements in center and to left of it. All props behaves similar to right
         currImgIndexOnRight = false;
-        currImgIndex = curr_center_copy;
-        if (curr_center_copy + 1 >= imgList.length) {
+        currImgIndex = curr_center_copy;  
+        if (curr_center_copy + 1 >= imgList.length) { // to maintain cyclic carousel
           curr_center_copy = 0;
         } else {
           curr_center_copy++;
@@ -61,20 +63,20 @@ const Carousel = (props) => {
         zTranslate =  - division * Math.abs(leftEltCount - (timesToIterate + 1));
         xTranslate = img_width + division * Math.abs(leftEltCount - (timesToIterate + 1));
       }
+      // Assigning above calculated values to 'styles' object
       styles.transform =  'translateX(' + xTranslate + 'px) translateZ(' +  zTranslate + 'px)';
       styles.opacity = opacity;
-      styles.zIndex = currImgIndexOnRight ? zIndex++ : zIndex --;
-      visibleItemsProps.order.push(currImgIndex);
-      visibleItemsProps[currImgIndex] = { styles };
+      styles.zIndex = currImgIndexOnRight ? zIndex++ : zIndex --; // To reduce zIndex while going through left elements
+      visibleItemsProps.order.push(currImgIndex); // Push the current image number in the orders array
+      visibleItemsProps[currImgIndex] = { styles }; // pushing all previously created styles
       timesToIterate++;
     }
-    durationRef.current = actualFirst === '' ? duration : ((duration / imgDifference.current));
-    console.log(visibleItemsProps);
-    setVisibleItemsProps(visibleItemsProps);
+    durationRef.current = actualFirst === '' ? duration : ((duration / imgDifference.current)); // duration will be user given if he clicks next image or we divide the duration by number of images skipped
+    setVisibleItemsProps(visibleItemsProps); // setting state for visible items
   }
 
 
-  const changeCenter = ({event, index, large_url}) => {
+  const changeCenter = ({event, index, large_url }) => {
     // Checking if the clicked item is immediately next/prev item.Because to induce a carousel effect we need to make the images move in sequence.
     // But that is not possible, when the user clicks out of sequence(Eg: image 4 from image 1).So here, the order breaks and we cannot 
     // apply our sequential handling logic to it. If we do the 4th image comes into view from the back but the "cycling effect"
@@ -129,6 +131,15 @@ const Carousel = (props) => {
     constructVisibleItemsProps(); // This constructs all css properties to the elements in visibility
     currMiddleImgRef.current = currFirstImg;  // Need to set it here as well so while accessing inside interval it will have the latest value
   }, [currFirstImg]);
+
+  useEffect(() => {
+    if (autoNext) {
+      setInterval(() => {
+        const nextImg = currMiddleImgRef.current + 1 < imgList.length ?  currMiddleImgRef.current + 1 : 0;
+        setCurrFirstImg(nextImg);
+      }, timeForNext);
+    }
+  }, []);
 
   const loadCarousel = () => {
     return (
